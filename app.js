5de8deb8aa3c435e7248e1d2034dc0182fecf183
abelan23 <abelan23@gmail.com>
2019-03-24 21:50:22 -0400

    Added GET, POST, PUT, and DELETE methods

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = new SimpleHTTP();
+
+//Get users
+// http.get("http://jsonplaceholder.typicode.com/users")
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+//User data
+const data = {
+  name: "John Wayne",
+  username: "jwayne",
+  email: "jwayne@gmail.com"
+};
+
+//Create User
+// http.post("http://jsonplaceholder.typicode.com/users", data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+//Update User
+// http.put("http://jsonplaceholder.typicode.com/users/2", data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+//Delete User
+http
+  .delete("http://jsonplaceholder.typicode.com/users/2")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
